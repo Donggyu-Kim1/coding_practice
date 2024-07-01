df7c3783dcdcd137d1d6c71eb8d75fa03e798609
Donggyu-Kim1 <kimdg000411@gmail.com>
2024-07-02 00:53:59 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.24 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,41 +1,25 @@
 def solution(survey, choices):
-    # 1
-    dict_mbti = {"R": 0, "T": 0, "F": 0, "C": 0, "J": 0, "M": 0, "A": 0, "N": 0}
-    survey_elements = ["RT", "TR", "FC", "CF", "MJ", "JM", "AN", "NA"]
-
-    # 2
-    for i in range(len(survey)):
-        for j in survey_elements:
-            if survey[i] == j:
-                if 1 <= choices[i] <= 3:
-                    dict_mbti[j[0]] += (4 - choices[i])
-                elif 5 <= choices[i] <= 7:
-                    dict_mbti[j[1]] += (choices[i] - 4)
-                else:
-                    pass
-
-    # 3
-    if dict_mbti['R'] < dict_mbti['T']:
-        dict_mbti.pop('R')
-    else:
-        dict_mbti.pop('T')
-
-    if dict_mbti['C'] < dict_mbti['F']:
-        dict_mbti.pop('C')
-    else:
-        dict_mbti.pop('F')
-
-    if dict_mbti['J'] < dict_mbti['M']:
-        dict_mbti.pop('J')
-    else:
-        dict_mbti.pop('M')
-
-    if dict_mbti['A'] < dict_mbti['N']:
-        dict_mbti.pop('A')
-    else:
-        dict_mbti.pop('N')
-
-    dict_key = list(dict_mbti.keys())
-    result = "".join(dict_key)
-
-    return result
+    # 1. 성격별 점수 딕셔너리 초기화
+    scores = {
+        'R': 0, 'T': 0,
+        'C': 0, 'F': 0,
+        'J': 0, 'M': 0,
+        'A': 0, 'N': 0
+    }
+    
+    # 2. 입력받은 survey와 choices를 기반으로 점수 합산
+    for s, choice in zip(survey, choices):
+        if choice < 4:
+            scores[s[0]] += 4 - choice
+        elif choice > 4:
+            scores[s[1]] += choice - 4
+    
+    # 3. 각 지표에서 더 높은 점수를 받은 성격 유형 결정
+    result = []
+    result.append('R' if scores['R'] >= scores['T'] else 'T')
+    result.append('C' if scores['C'] >= scores['F'] else 'F')
+    result.append('J' if scores['J'] >= scores['M'] else 'M')
+    result.append('A' if scores['A'] >= scores['N'] else 'N')
+    
+    # 4. 결과를 문자열로 변환하여 반환
+    return ''.join(result)
