ae2da0be1d10009da06097dc381b8976636aaf0a
Donggyu-Kim1 <kimdg000411@gmail.com>
2024-07-16 16:37:07 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -2,7 +2,8 @@ def solution(n, m):
     min_list = []
     max_list = []
     answer = []
-    for i in range(1, m+1):
+    num = max(n,m)
+    for i in range(1, num+1):
         if n % i == 0 and m % i == 0:
             min_list.append(i)
             max_list.append(n/i*m/i*i)
