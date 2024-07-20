532446eaf7fba96719a699c4d0e35b33a3fa54cc
Donggyu-Kim1 <kimdg000411@gmail.com>
2024-07-20 14:51:04 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,6 @@
+def solution(array, commands):
+    answer = []
+    for i in commands:
+        sorted_arr = sorted(array[i[0]-1:i[1]])
+        answer.append(sorted_arr[i[2]-1])
+    return answer
