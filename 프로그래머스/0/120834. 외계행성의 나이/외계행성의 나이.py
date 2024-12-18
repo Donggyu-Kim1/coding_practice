8b6277df776ff90a20383c6f58acf3ff8105a88f
Donggyu-Kim <kimdg000411@gmail.com>
2024-12-19 01:12:39 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -0,0 +1,17 @@
+def solution(age):
+    answer = ''
+    code = {
+        "0" : "a",
+        "1" : "b",
+        "2" : "c",
+        "3" : "d",
+        "4" : "e",
+        "5" : "f",
+        "6" : "g",
+        "7" : "h",
+        "8" : "i",
+        "9" : "j",
+    }
+    for i in str(age):
+        answer += code[i] 
+    return answer
