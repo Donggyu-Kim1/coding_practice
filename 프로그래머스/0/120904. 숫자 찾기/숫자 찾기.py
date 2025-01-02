d254c7ec9e7ec5d56f354937a4bf063a7a2225a5
Donggyu-Kim <kimdg000411@gmail.com>
2025-01-03 00:12:55 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,13 +1,7 @@
 def solution(num, k):
-    answer = 0
-    i = 0
-    str_num = str(num)
     try:
-        while str(k) != str_num[i]:
-            i += 1
-        
-        return str_num.index(str_num[i]) + 1
-    except:
+        return str(num).index(str(k)) + 1
+    except ValueError:
         return -1
     
     
